fc1d3d64f893d45c07e8deb2da6650d410dbce52
huynhdat96 <huynhdat96@gmail.com>
2016-10-12 09:37:14 +0700

    Thêm textbox vô app

--- vdv_dictionary/Form1.Designer.cs
+++ vdv_dictionary/Form1.Designer.cs
@@ -29,32 +29,43 @@
         private void InitializeComponent()
         {
             this.click_here = new System.Windows.Forms.Button();
+            this.enter = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // click_here
             // 
-            this.click_here.Location = new System.Drawing.Point(249, 139);
+            this.click_here.Location = new System.Drawing.Point(21, 12);
             this.click_here.Name = "click_here";
             this.click_here.Size = new System.Drawing.Size(129, 50);
             this.click_here.TabIndex = 0;
             this.click_here.Text = "Click Here";
             this.click_here.UseVisualStyleBackColor = true;
             // 
+            // enter
+            // 
+            this.enter.Location = new System.Drawing.Point(21, 68);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(604, 26);
+            this.enter.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(645, 338);
+            this.Controls.Add(this.enter);
             this.Controls.Add(this.click_here);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button click_here;
+        private System.Windows.Forms.TextBox enter;
 
     }
 }
